3ad0a6240e1a847a24cf2f554e169cf82293a1ee
Atsushi Honda <athonda@c1r3s3.42singapore.sg>
2025-06-09 18:43:27 +0800

    style: clean up and add comment

--- client.cpp
+++ client.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 15:29:12 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/09 15:31:46 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/09 17:47:34 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <sys/socket.h> // socket
+#include <netinet/in.h> // sockaddr_in, sockaddr
+#include <arpa/inet.h> // inet_pton()
 
 int main()
 {
@@ -24,15 +26,14 @@ int main()
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(12345);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(12345);
+	inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-    connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    write(sockfd, "Hello, server!", 15);
+	write(sockfd, "Hello, server!", 15);
+	close(sockfd);
 
-    close(sockfd);
-
-    return 0;
+	return 0;
 }
