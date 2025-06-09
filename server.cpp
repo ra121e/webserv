5a4a685f1f4b47c72ce3396d0491da2d27435ca6
Atsushi Honda <athonda@c1r3s3.42singapore.sg>
2025-06-09 14:58:50 +0800

    rename: main.cpp -> server.cpp

--- server.cpp
+++ server.cpp
@@ -6,14 +6,32 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 13:04:22 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/09 13:04:53 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/09 13:55:38 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+#include <sys/socket.h> // socket
+#include <netinet/in.h> // AF_INET, sockaddr_in
+#include <errno.h> // perror
+#include <stdio.h> // perror
+#include <unistd.h> // close(sock)
+
+void	printBanner(std::string const &title)
+{
+	std::cout << "\n--- " << title << " ---" << std::endl;
+}
 
 int	main(void)
 {
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0)
+		perror("socket");
+	std::cout << "socket return value is " << sock << std::endl;
+
+	printBanner("test: write() send message to socket");	
+	write(3, "a", 1);
+	close(sock);
 	return (0);
 }
